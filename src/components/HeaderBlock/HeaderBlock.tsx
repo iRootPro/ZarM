5e9f8655886b0fc643e794d3f4c30d9474cf9111
Alexander Neupokoev <admin@armavir.ru>
2020-12-20 18:37:45 +0300

    Image

--- src/components/HeaderBlock/HeaderBlock.tsx
+++ src/components/HeaderBlock/HeaderBlock.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import style from './HeaderBlock.module.css'
+import ReactLogo from './img/logo.png'
 
 const HeaderBlock = () => {
     return (
-        <div>
-            <h1 className={style.header}>Учите английские слова Online</h1>
-            <p>Воспользуйтесь карточками</p>
+        <div className={style.cover}>
+            <div className={style.wrap}>
+                <h1 className={style.header}>Учите слова онлайн</h1>
+                <p className={style.descr}>Воспользуйтесь карточками для запоминания и пополнения активныйх словарных
+                    запасов</p>
+                <img src={ReactLogo} alt="React logo"/>
+            </div>
         </div>
     );
 };
